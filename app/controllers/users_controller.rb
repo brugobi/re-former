ea1a850fdb27f95ef8f34324ff0d53f836f3da9c
brugobi <brgobbi@hotmail.com>
2020-07-24 17:21:10 -0400

    add edit and destroy methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
     def new
         @user = User.new
     end
@@ -7,16 +15,32 @@ class UsersController < ApplicationController
         @user = User.new(user_param)
 
         if @user.save
-            redirect_to new_user_path
+            redirect_to users_path
         else
             render :new
         end
     end
 
     def edit
-        @user = User.find(paramas[:id])
+        @user = User.find(params[:id])
     end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_param)
+
+        flash.notice = "User '#{@user.username}' Updated!"
+  
+        redirect_to users_path
+    end 
     
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+
+        redirect_to users_path
+    end 
+
     private
     def user_param
         params.require(:user).permit(:username,:email,:password)
